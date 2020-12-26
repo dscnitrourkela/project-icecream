c2476584a7e0b656ea49f70e2a8fb264c27d4eee
Ritesh Patil <riteshsp2000@gmail.com>
2020-12-26 23:49:55 +0530

    Add image input with max limit of 10mb

--- react-profile/src/components/App.tsx
+++ react-profile/src/components/App.tsx
@@ -1,11 +1,27 @@
-import * as React from 'react'
+import React, {useState} from 'react'
 import FrameCarousel from './carousel/Carousel'
+import ImageUploader from 'react-images-upload';
+
+
 
 const App: React.FC = () => {
+  const [image, setImage] = useState<undefined | File>()
+
   return (
     <div>
       <h1>Hello Typescript!</h1>
       <FrameCarousel />
+      <ImageUploader
+        withIcon={false}
+        withLabel={false}
+        buttonText='Choose images'
+        onChange={(picture) => setImage(picture[0])}
+        imgExtension={['.jpg', '.gif', '.png', '.gif']}
+        maxFileSize={5242880 * 2}
+      />
+      {image && (
+        <img src={URL.createObjectURL(image)} alt='imgae' />
+      )}
     </div>
   )
 }
