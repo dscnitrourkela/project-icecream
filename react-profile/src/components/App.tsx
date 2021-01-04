9061310f8175527f9aefeffc8f9f8134f5412225
Ritesh Patil <riteshsp2000@gmail.com>
2021-01-04 16:53:49 +0530

    Fix cropper issues

--- react-profile/src/components/App.tsx
+++ react-profile/src/components/App.tsx
@@ -3,7 +3,6 @@ import ImageUploader from 'react-images-upload';
 import {makeStyles} from '@material-ui/core'
 import Cropper from 'react-easy-crop'
 
-import Modal from './Modal'
 
 // import FrameCarousel from './carousel/Carousel'
 
@@ -13,16 +12,6 @@ const App: React.FC = () => {
   const [zoom, setZoom] = useState<number>(1)
   const [aspect, setAspect] = useState<number>(1/1)
 
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
     <div>
       <h1>Hello Typescript!</h1>
@@ -30,22 +19,31 @@ const App: React.FC = () => {
         withIcon={false}
         withLabel={false}
         buttonText='Choose images'
-        onChange={async (picture) => {
-          handleOpen()
-          setImage(picture[0])
-        }}
+        onChange={async (picture) => {setImage(picture[0])}}
         imgExtension={['.jpg', '.gif', '.png', '.gif', '.jpeg']}
         maxFileSize={5242880 * 2}
         singleImage={true}
       />
-      {/* {image && ( */}
-        <Modal open={open} handleClose={handleClose}>
+      {image && (
+        <div
+        style={{
+          height: 576 + 64,
+          width: 576 + 64,
+          overflow: 'hidden',
+          backgroundImage: 'url(/frame.png)',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          position: 'relative',
+        }}
+      >
           <Cropper
               image={image ? URL.createObjectURL(image) : ''}
               crop={crop}
               zoom={zoom}
               aspect={aspect}
               minZoom={1}
+              restrictPosition={false}
+              cropSize={{ width: 512, height: 512 }}
               onCropChange={(crop: {x: number, y: number}) => setCrop(crop)}
               onCropComplete={() => console.log(crop)}
               onZoomChange={(zoom: number) => setZoom(zoom)}
@@ -53,27 +51,22 @@ const App: React.FC = () => {
                 containerStyle: {
                   width: 512,
                   height: 512,
-                  // overflow: 'hidden',
-                  // marginTop: 8,
+                  overflow: 'hidden',
                   position: 'absolute',
-                  top: 0,
-                  left: 0
+                  top: 64,
+                  left: 64,
                 },
                 mediaStyle: {
-                  // objectFit: 'cover',
-                  // objectPosition: 'center top',
+                  objectFit: 'cover',
+                  objectPosition: 'center top',
                 },
                 cropAreaStyle: {
-                  // backgroundImage: 'url(/frame.png)',
-                  // backgroundPosition: 'center',
-                  // backgroundSize: 'cover',
-                  // width: 600,
-                  // height: 600,
+                  boxShadow: '0 0 0 0',
                 },
               }}
-          />
-        </Modal>
-      {/* // )} */}
+          /> 
+        </div>
+      )}
       
     </div>
   )
