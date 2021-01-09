4a8cb4d8baabd4d610f25411f575968e23dc319a
Ritesh Patil <riteshsp2000@gmail.com>
2021-01-09 22:34:09 +0530

    Add custom text box inside frames (#15)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import './index.css';
+import App from './views/App';
 
 import configureFirebase from './config/firebase';
 
-configureFirebase().then(async () => {
-  const App = await import('./views/App');
-  ReactDOM.render(<App.default />, document.getElementById('root'));
+configureFirebase().then(() => {
+  // const App = await import('./views/App');
+  ReactDOM.render(<App />, document.getElementById('root'));
 });
