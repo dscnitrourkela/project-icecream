9b23c5d9ef5f25d4c278f6fa8ccaa645f0514549
CIPHERTron <pritish.samal918@gmail.com>
2021-07-01 20:27:08 +0530

    feat: Add INIT Frames

--- client/src/config/data.ts
+++ client/src/config/data.ts
@@ -2,23 +2,79 @@ export const frameData = [
 	{
 		approved: true,
 		backgroundColor: {
-			color1: "#f12711",
-			color2: "#f5af19",
+			color1: "#FCC989",
+			color2: "#98F2E2",
 			type: "linear",
 		},
 		dimensions: {
-			bottom: 490,
-			height: 4500,
+			bottom: 245,
+			height: 2250,
+			left: 245,
+			right: 245,
+			top: 245,
+			width: 2250,
+		},
+		frame: "https://res.cloudinary.com/pritish007/image/upload/v1625150153/Frames/MLH_INIT_GREEN_u5gwqg.png",
+		name: "MLH INIT 1",
+		renderDimensions: {
+			bottom: 71.27272727272727,
+			height: 654.5454545454545,
+			left: 71.27272727272727,
+			right: 71.27272727272727,
+			top: 71.27272727272727,
+			width: 654.5454545454545,
+		},
+		shape: "square",
+		showTextBox: true,
+	},
+	{
+		approved: true,
+		backgroundColor: {
+			color1: "#FCC989",
+			color2: "#98F2E2",
+			type: "linear",
+		},
+		dimensions: {
+			bottom: 245,
+			height: 2250,
+			left: 245,
+			right: 245,
+			top: 245,
+			width: 2250,
+		},
+		frame: "https://res.cloudinary.com/pritish007/image/upload/v1625150153/Frames/MLH_INIT_PINK_rlxp5p.png",
+		name: "MLH INIT 2",
+		renderDimensions: {
+			bottom: 71.27272727272727,
+			height: 654.5454545454545,
+			left: 71.27272727272727,
+			right: 71.27272727272727,
+			top: 71.27272727272727,
+			width: 654.5454545454545,
+		},
+		shape: "square",
+		showTextBox: true,
+	},
+	{
+		approved: true,
+		backgroundColor: {
+			color1: "#025955",
+			color2: "#025955",
+			type: "linear",
+		},
+		dimensions: {
+			bottom: 470,
+			height: 4501,
 			left: 490,
 			right: 490,
 			top: 470,
-			width: 4500,
+			width: 4501,
 		},
-		frame: "https://res.cloudinary.com/pritish007/image/upload/v1616326076/Almost_final_frame_p45ci7.png",
-		name: "HackNITR 2.0",
+		frame: "https://res.cloudinary.com/dalqfvowk/image/upload/v1610220397/icecream_frames/hd9zlgcsw9zjppn0lw9x.png",
+		name: "DSC NIT Rourkela",
 		renderDimensions: {
-			bottom: 71.03057757644395,
-			height: 652.321630804077,
+			bottom: 71.27272727272727,
+			height: 654.5454545454545,
 			left: 71.27272727272727,
 			right: 71.27272727272727,
 			top: 69.29105322763307,
@@ -86,23 +142,23 @@ export const frameData = [
 	{
 		approved: true,
 		backgroundColor: {
-			color1: "#025955",
-			color2: "#025955",
+			color1: "#f12711",
+			color2: "#f5af19",
 			type: "linear",
 		},
 		dimensions: {
-			bottom: 470,
-			height: 4501,
+			bottom: 490,
+			height: 4500,
 			left: 490,
 			right: 490,
 			top: 470,
-			width: 4501,
+			width: 4500,
 		},
-		frame: "https://res.cloudinary.com/dalqfvowk/image/upload/v1610220397/icecream_frames/hd9zlgcsw9zjppn0lw9x.png",
-		name: "DSC NIT Rourkela",
+		frame: "https://res.cloudinary.com/pritish007/image/upload/v1616326076/Almost_final_frame_p45ci7.png",
+		name: "HackNITR 2.0",
 		renderDimensions: {
-			bottom: 71.27272727272727,
-			height: 654.5454545454545,
+			bottom: 71.03057757644395,
+			height: 652.321630804077,
 			left: 71.27272727272727,
 			right: 71.27272727272727,
 			top: 69.29105322763307,
@@ -112,33 +168,3 @@ export const frameData = [
 		showTextBox: true,
 	},
 ]
-
-// JSON Format
-// {
-//  approved: true,
-//  backgroundColor: {
-//   color1: "",
-//   color2: "",
-//   type: "linear"
-//  },
-//  dimensions: {
-//   bottom: 490,
-//   height: 4500,
-//   left: 490,
-//   right: 490,
-//   top: 470,
-//   width: 4500
-//  },
-//  frame: "",
-//  name: "HackNITR 2.0",
-//  renderDimensions: {
-//   bottom: 1,
-//   height: 1,
-//   left: 1,
-//   right: 1,
-//   top: 1,
-//   width: 1
-//  },
-//  shape: "square",
-//  showTextBox: true
-// }
