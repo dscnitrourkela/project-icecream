2b0bc8a7e5dd51cbbe69635db82c654f0ff200d3
Sonakshi1901 <sonakshi1901@gmail.com>
2021-10-28 18:14:14 +0400

    fix: changed the cloudinary links

--- config/frameData.js
+++ config/frameData.js
@@ -1,9 +1,9 @@
 /* eslint-disable max-len */
 export default {
   frames: {
-    ONE: 'https://res.cloudinary.com/sambitsankalp/image/upload/v1635422249/hacknitr/frameboi/frame-1_sjbetr.png',
-    TWO: 'https://res.cloudinary.com/sambitsankalp/image/upload/v1635422248/hacknitr/frameboi/frame-2_iizwfw.png',
-    THREE: 'https://res.cloudinary.com/sambitsankalp/image/upload/v1635422248/hacknitr/frameboi/frame-3_qgnr0h.png',
+    ONE: 'https://res.cloudinary.com/sambitsankalp/image/upload/v1635430347/hacknitr/frameboi/frame-1_hdumwb.png',
+    TWO: 'https://res.cloudinary.com/sambitsankalp/image/upload/v1635430347/hacknitr/frameboi/frame-2_wvkuud.png',
+    THREE: 'https://res.cloudinary.com/sambitsankalp/image/upload/v1635430347/hacknitr/frameboi/frame-3_e8eseh.png',
     FOUR: '',
     FIVE: '',
     SIX: '',
