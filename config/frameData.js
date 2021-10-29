6d29c85780d588aa84be379c364dabaa2accc742
Sonakshi1901 <sonakshi1901@gmail.com>
2021-10-29 13:06:17 +0400

    fix: removed unused frames

--- config/frameData.js
+++ config/frameData.js
@@ -4,9 +4,5 @@ export default {
     ONE: 'https://res.cloudinary.com/sambitsankalp/image/upload/v1635430347/hacknitr/frameboi/frame-1_hdumwb.png',
     TWO: 'https://res.cloudinary.com/sambitsankalp/image/upload/v1635430347/hacknitr/frameboi/frame-2_wvkuud.png',
     THREE: 'https://res.cloudinary.com/sambitsankalp/image/upload/v1635430347/hacknitr/frameboi/frame-3_e8eseh.png',
-    FOUR: '',
-    FIVE: '',
-    SIX: '',
-    SEVEN: ''
   },
 };
