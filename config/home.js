07c115a6649a364fc7bfcee542c62f5518775c8e
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2022-08-14 22:34:21 +0530

    fix : image links fixed and content updated (#71)

--- config/home.js
+++ config/home.js
@@ -1,17 +1,17 @@
 /* eslint-disable max-len */
 export default {
   logo: {
-    src: 'https://res.cloudinary.com/riteshsp2000/image/upload/project-oregano/assets/hkmogkgvx6kpf07xbln8.png',
+    src: 'https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/logo1_rg97i6.png',
     alt: 'logo',
-    web: 'https://hacknitr.tech/',
+    web: 'https://hacknitr.com/',
   },
   frame: {
     src: 'https://res.cloudinary.com/sambitsankalp/image/upload/v1635351154/hacknitr/frameboi/home-image_fzkv5b.png',
     alt: 'frame',
   },
-  head: 'HackNITR 3.0',
+  head: 'HackNITR 4.0',
   description:
-    "All of us love to test out different exciting options, isn't it? Don't worry, HackNITR 3.0 has got you covered. To keep up with this, HackNITR 3.0 offers you FRAMEBOI, a website with a unique variety of frames to create your own badges. Designed with love, keeping in mind the hackathon style, this frame would help you experiment with different options available. Feel free to share it on your social media and don't forget to tag us!",
-  subheading: 'HackNITR 3.0 Presents',
+    "All of us love to test out different exciting options, isn't it? Don't worry, HackNITR 4.0 has got you covered. To keep up with this, HackNITR 4.0 offers you FRAMEBOI, a website with a unique variety of frames to create your own badges. Designed with love, keeping in mind the hackathon style, this frame would help you experiment with different options available. Feel free to share it on your social media and don't forget to tag us!",
+  subheading: 'HackNITR 4.0 Presents',
   button: 'continue',
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -80,8 +80,8 @@ const SlimText = styled.span`
 const Home = () => (
   <>
     <Helmet>
-      <title>Frameboi | HackNITR 3.0</title>
-      <meta name='Frameboi | HackNITR 3.0' contect='Virtual badges for HackNITR 3.0' />
+      <title>Frameboi | HackNITR 4.0</title>
+      <meta name='Frameboi | HackNITR 4.0' contect='Virtual badges for HackNITR 4.0' />
     </Helmet>
     <Container>
       <Head />
